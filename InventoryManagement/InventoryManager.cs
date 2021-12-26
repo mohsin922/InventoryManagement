4f2f8548843d4bc57c6afbcc32a6017372d8e1f2
MOHSIN ZAHOOR PEER <59727449+mohsin922@users.noreply.github.com>
2021-12-26 13:39:03 +0530

    Update InventoryManager.cs

--- InventoryManagement/InventoryManager.cs
+++ InventoryManagement/InventoryManager.cs
@@ -1,35 +1,3 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-
-//namespace InventoryMangement
-//{
-//    class InventoryManager
-//    {
-//        public void DisplayInventory(List<InventoryUtility.Rice> Ricelist)
-//        {
-//            foreach (InventoryUtility.Rice R in Ricelist)
-//            {
-//                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", R.Name, R.Kg, R.Price, R.Type);
-//            }
-
-//        }
-//        public void DisplayInventory(List<InventoryUtility.Wheat> WheatList)
-//        {
-//            foreach (InventoryUtility.Wheat W in WheatList)
-//            {
-//                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", W.Name, W.Kg, W.Price, W.Type);
-//            }
-//        }
-//        public void DisplayInventory(List<InventoryUtility.Pulse> PulseList)
-//        {
-//            foreach (InventoryUtility.Pulse P in PulseList)
-//            {
-//                Console.WriteLine("Name: {0} \nKg: {1} \nPrice: {2} \nType:{3}\n", P.Name, P.Kg, P.Price, P.Type);
-//            }
-//        }
-//    }
-//}
 using System;
 using System.Collections.Generic;
 using System.Text;
